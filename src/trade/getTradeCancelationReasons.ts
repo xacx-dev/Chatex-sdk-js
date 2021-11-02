17f0c441721d7d39a8e516a683bbe146ee8fa4d2
Mikahil Nagornyi <thexacx@vk.com>
2021-11-02 03:09:50 +0300

    fix slash bug

--- src/trade/getTradeCancelationReasons.ts
+++ src/trade/getTradeCancelationReasons.ts
@@ -4,7 +4,7 @@ export interface GetTradeCancelationReasons {
     (): Promise<string[]>;
 }
 export const getTradeCancelationReasons: GetTradeCancelationReasons = async (): Promise<string[]> => {
-    return fetchApi({url: 'exchange/adverts/trades/cancelation-reasons', method: 'GET'})
+    return fetchApi({url: '/exchange/adverts/trades/cancelation-reasons', method: 'GET'})
         .then(response => response.json());
 };
 
